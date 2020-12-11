6fefa0030a7ad102bcd8b2cd015a0ddd4128048a
jfspps <jfsapps@gmail.com>
2020-12-11 13:56:49 +0000

    first commit; project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppinglisteditComponent } from './shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipeitemComponent } from './recipebook/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipebook/recipedetail/recipedetail.component';
+import { ShoppingListComponent } from './shoppinglist/shoppinglist/shoppinglist.component';
+import { RecipeList } from './recipebook/recipelist/recipelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppinglisteditComponent,
+    RecipeList,
+    RecipeitemComponent,
+    RecipedetailComponent
   ],
   imports: [
     BrowserModule,
