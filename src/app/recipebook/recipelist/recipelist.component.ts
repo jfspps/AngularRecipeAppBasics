97b233a75641c0b3b000f0dcb5605300a62f2f8a
jfspps <jfsapps@gmail.com>
2020-12-11 15:35:13 +0000

    rendered recipes array to html

--- src/app/recipebook/recipelist/recipelist.component.ts
+++ src/app/recipebook/recipelist/recipelist.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
     selector: 'app-recipelist',
     templateUrl: './recipelist.component.html'
 })
 export class RecipeList {
-    recipes = [];   // see recipe.model.ts for definition
+    recipes: Recipe[] = [
+        new Recipe('test recipe name', 'some description', 
+        'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+    ];   // see recipe.model.ts for definition
 
 }
