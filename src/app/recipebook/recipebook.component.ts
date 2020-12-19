ec6c61934c85960fd94c66c34103001d8126f364
jfspps <jfsapps@gmail.com>
2020-12-19 16:38:25 +0000

    passing recipe to >1 other component

--- src/app/recipebook/recipebook.component.ts
+++ src/app/recipebook/recipebook.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
     selector: 'app-recipebook',
@@ -6,4 +7,5 @@ import { Component } from "@angular/core";
 })
 export class RecipeBook{
 
+    selectedRecipe: Recipe;
 }
--- src/app/recipebook/recipelist/recipelist.component.ts
+++ src/app/recipebook/recipelist/recipelist.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -6,9 +6,17 @@ import { Recipe } from "../recipe.model";
     templateUrl: './recipelist.component.html'
 })
 export class RecipeList {
+
+    @Output() recipeSelection = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
         new Recipe('test recipe name', 'some description', 
+        'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+        new Recipe('test recipe name #2', 'some description', 
         'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
     ];   // see recipe.model.ts for definition
 
+    onRecipeSelected(recipe: Recipe){
+        this.recipeSelection.emit(recipe);
+    }
 }
